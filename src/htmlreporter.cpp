53feea159f679a08edc7cb01ae49d180762305f6
Cameron Hyde <42562517+neoformit@users.noreply.github.com>
2021-12-15 09:58:01 +1000

    Update plotly.js URL to HTTPS

--- src/htmlreporter.cpp
+++ src/htmlreporter.cpp
@@ -404,7 +404,7 @@ void HtmlReporter::printCSS(ofstream& ofs){
 }
 
 void HtmlReporter::printJS(ofstream& ofs){
-    ofs << "<script src='http://opengene.org/plotly-1.2.0.min.js'></script>" << endl;
+    ofs << "<script src='https://opengene.org/plotly-1.2.0.min.js'></script>" << endl;
     ofs << "\n<script type='text/javascript'>" << endl;
     ofs << "    window.Plotly || document.write('<script src=\"https://cdn.plot.ly/plotly-1.2.0.min.js\"><\\/script>')" << endl;
     ofs << "</script>" << endl;
